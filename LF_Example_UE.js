f497790a5e9767c512769057acc613272650248a
Lukasz Formela <39266504+LukaszFormela@users.noreply.github.com>
2022-04-07 17:02:18 +0100

    Create LF_Example_UE.js

---
+++ LF_Example_UE.js
@@ -0,0 +1,33 @@
+/**
+ * Example User Event usage
+ *
+ * LF_Example_UE.js
+ *
+ * @author Lukasz Formela <hello@lukaszformela.com>
+ * @website lukaszformela.com
+ * @NApiVersion 2.1
+ * @NScriptType UserEventScript
+ */
+define([
+  'N/runtime',
+  'LF_Helpers.js',
+], function (_runtime, _helpers) {
+  const exports = {};
+  
+  /**
+   * Function definition to be triggered before record is submitted.
+   *
+   * @param {Object} scriptContext Context of a script
+   *
+   * @returns {void}
+   */
+  const beforeSubmit = (scriptContext) => {
+    const currentScript = _runtime.getCurrentScript();
+    
+    _helpers.setMrScriptKillswitch(currentScript.id);
+  }
+  
+  exports.beforeSubmit = beforeSubmit;
+  
+  return exports;
+}
